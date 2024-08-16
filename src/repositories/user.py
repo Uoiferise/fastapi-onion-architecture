155a89511b9cbff93f6a74f52cf8a683a60d7386
kalyukov.ns <ws555w@mail.ru>
2024-08-16 20:26:46 +0300

    refactor: add get users by filters function to user repository

--- src/repositories/user.py
+++ src/repositories/user.py
@@ -1,6 +1,30 @@
+from collections.abc import Sequence
+
+from sqlalchemy import Result, select
+
 from src.models import UserModel
+from src.schemas.user import UserFilters
 from src.utils.repository import SqlAlchemyRepository
 
 
 class UserRepository(SqlAlchemyRepository):
     model = UserModel
+
+    async def get_users_by_filter(self, filters: UserFilters) -> Sequence[UserModel]:
+        """Find all users by filters."""
+        query = select(self.model)
+
+        if filters.ids:
+            query = query.where(UserModel.id.in_(filters.ids))
+
+        if filters.first_name:
+            query = query.where(UserModel.first_name.in_(filters.first_name))
+
+        if filters.last_name:
+            query = query.where(UserModel.last_name.in_(filters.last_name))
+
+        if filters.middle_name:
+            query = query.where(UserModel.middle_name.in_(filters.middle_name))
+
+        res: Result = await self.session.execute(query)
+        return res.scalars().all()
