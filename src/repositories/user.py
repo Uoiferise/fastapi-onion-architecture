0f7994acf14a35a70e7b955e21ac277440c0e7c7
kalyukov.ns <ws555w@mail.ru>
2024-08-17 00:46:19 +0300

    rework: repositories

--- src/repositories/user.py
+++ src/repositories/user.py
@@ -15,16 +15,16 @@ class UserRepository(SqlAlchemyRepository):
         query = select(self.model)
 
         if filters.ids:
-            query = query.where(UserModel.id.in_(filters.ids))
+            query = query.where(self.model.id.in_(filters.ids))
 
         if filters.first_name:
-            query = query.where(UserModel.first_name.in_(filters.first_name))
+            query = query.where(self.model.first_name.in_(filters.first_name))
 
         if filters.last_name:
-            query = query.where(UserModel.last_name.in_(filters.last_name))
+            query = query.where(self.model.last_name.in_(filters.last_name))
 
         if filters.middle_name:
-            query = query.where(UserModel.middle_name.in_(filters.middle_name))
+            query = query.where(self.model.middle_name.in_(filters.middle_name))
 
         res: Result = await self.session.execute(query)
         return res.scalars().all()
