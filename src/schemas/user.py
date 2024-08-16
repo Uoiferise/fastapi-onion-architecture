bba0492fcb8fbc24840b35c80c154927118aff4f
kalyukov.ns <ws555w@mail.ru>
2024-08-16 20:25:39 +0300

    refactor: base user schemas

--- src/schemas/user.py
+++ src/schemas/user.py
@@ -1,21 +1,45 @@
+from dataclasses import dataclass
+
+from fastapi import Query
 from pydantic import UUID4, BaseModel, Field
 
+from src.schemas.filter import TypeFilter
+from src.schemas.response import BaseCreateResponse, BaseResponse
 
-class IdUserSchema(BaseModel):
+
+class UserID(BaseModel):
     id: UUID4
 
 
-class CreateUserSchema(BaseModel):
+class CreateUserRequest(BaseModel):
     first_name: str = Field(max_length=50)
     last_name: str = Field(max_length=50)
     middle_name: str | None = Field(max_length=50, default=None)
 
 
-class UserSchema(IdUserSchema, CreateUserSchema):
+class UpdateUserRequest(CreateUserRequest):
     pass
 
 
-class UpdateUserSchema(IdUserSchema):
-    first_name: str | None = Field(max_length=50, default=None)
-    last_name: str | None = Field(max_length=50, default=None)
-    middle_name: str | None = Field(max_length=50, default=None)
+class UserDB(UserID, CreateUserRequest):
+    pass
+
+
+class CreateUserResponse(BaseCreateResponse):
+    payload: UserDB
+
+
+class UserResponse(BaseResponse):
+    payload: UserDB
+
+
+class UsersListResponse(BaseResponse):
+    payload: list[UserDB]
+
+
+@dataclass
+class UserFilters(TypeFilter):
+    ids: list[UUID4] | None = Query(None)
+    first_name: list[str] | None = Query(None)
+    last_name: list[str] | None = Query(None)
+    middle_name: list[str] | None = Query(None)
