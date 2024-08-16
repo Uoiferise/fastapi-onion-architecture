01c6a3e247395e1053cedfa334afaecf06705360
kalyukov.ns <ws555w@mail.ru>
2024-08-16 20:24:58 +0300

    add: base response schemas

---
+++ src/schemas/response.py
@@ -0,0 +1,13 @@
+
+from pydantic import BaseModel
+from starlette.status import HTTP_200_OK, HTTP_201_CREATED
+
+
+class BaseResponse(BaseModel):
+    status: int = HTTP_200_OK
+    error: bool = False
+
+
+class BaseCreateResponse(BaseModel):
+    status: int = HTTP_201_CREATED
+    error: bool = False
