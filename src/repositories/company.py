d0a166edeb5128bb733da803a1a55107c96ad978
kalyukov.ns <ws555w@mail.ru>
2025-05-04 16:55:53 +0300

    refactor: rework user and company repositories

--- src/repositories/company.py
+++ src/repositories/company.py
@@ -7,14 +7,14 @@ from src.utils.repository import SqlAlchemyRepository
 
 
 class CompanyRepository(SqlAlchemyRepository):
-    model = CompanyModel
+    _model = CompanyModel
 
     async def get_company_with_users(self, company_id: UUID4) -> CompanyModel | None:
         """Find company by ID with all users."""
         query = (
-            select(self.model)
-            .where(self.model.id == company_id)
-            .options(selectinload(self.model.users))
+            select(self._model)
+            .where(self._model.id == company_id)
+            .options(selectinload(self._model.users))
         )
         res: Result = await self._session.execute(query)
         return res.scalar_one_or_none()
--- src/repositories/user.py
+++ src/repositories/user.py
@@ -8,23 +8,23 @@ from src.utils.repository import SqlAlchemyRepository
 
 
 class UserRepository(SqlAlchemyRepository):
-    model = UserModel
+    _model = UserModel
 
     async def get_users_by_filter(self, filters: UserFilters) -> Sequence[UserModel]:
         """Find all users by filters."""
-        query = select(self.model)
+        query = select(self._model)
 
         if filters.ids:
-            query = query.where(self.model.id.in_(filters.ids))
+            query = query.where(self._model.id.in_(filters.ids))
 
         if filters.first_name:
-            query = query.where(self.model.first_name.in_(filters.first_name))
+            query = query.where(self._model.first_name.in_(filters.first_name))
 
         if filters.last_name:
-            query = query.where(self.model.last_name.in_(filters.last_name))
+            query = query.where(self._model.last_name.in_(filters.last_name))
 
         if filters.middle_name:
-            query = query.where(self.model.middle_name.in_(filters.middle_name))
+            query = query.where(self._model.middle_name.in_(filters.middle_name))
 
         res: Result = await self._session.execute(query)
         return res.scalars().all()
